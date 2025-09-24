953f317048c50c791bc3e1fe18178552a3a7f60a
sadowiskidavis <davissadowiskidasilva@gmail.com>
2025-09-23 21:50:56 -0300

    atualizando

--- 22_09/src/index.ts
+++ 22_09/src/index.ts
@@ -1,8 +1,9 @@
+import { getJSDocCommentsAndTags, isForInitializer } from "typescript";
 import { DiaDaSemana } from "./DiaDaSemana"
 import { Pizza } from "./Pizza"
 import { SaborPizza } from "./SaborPizza"
 import { TamanhoPizza } from "./TamanhoPizza"
-
+import * as readline from 'readline-sync';
 
 
 function verificaDia(dia: DiaDaSemana){
@@ -21,3 +22,64 @@ const pedido1:Pizza = new Pizza(SaborPizza.calabresa,TamanhoPizza.pequena)
 
 
 console.log(pedido1.descricao())
+
+
+function saborEscolhido(){
+    Object.values(SaborPizza).forEach((sabor:SaborPizza, index:number) =>{
+        console.log(`${index + 1}. ${sabor}`)
+    })
+
+
+const escolha = readline.questionInt("Digite o numero do sabor: ")
+const valores = Object.values(SaborPizza)
+const saborescolhido = valores[escolha - 1]
+
+return saborescolhido as SaborPizza
+}
+
+
+function tamanhoEscolhido(){
+    Object.values(TamanhoPizza).forEach((tamanho:TamanhoPizza, index:number) =>{
+        console.log(`${index + 1}. ${tamanho}`)
+    })
+
+
+const escolha = readline.questionInt("Digite o numero do sabor: ")
+const valores = Object.values(TamanhoPizza)
+const tamanhoescolhido = valores[escolha - 1]
+
+return tamanhoescolhido as TamanhoPizza
+}
+
+
+function fazerPedido():void {
+    let opcao:number = 0
+    while(opcao !==2){
+        
+        opcao = readline.questionInt("==== MENU PIZZARIA ==== \n1- Fazer Pedido \n2- sair ");
+    
+        switch(opcao){
+    
+        case 1:
+            const sabor = saborEscolhido()
+            const tamanho = tamanhoEscolhido()
+            const pizza:Pizza = new Pizza(sabor, tamanho)
+            console.log(pizza.descricao())
+    
+        break;
+        case 2:
+            console.log("Saindo... ")
+            break;
+            default:
+                console.log("Opção invalida. ")
+                break
+    
+    }
+    
+    
+    }
+}
+
+
+
+fazerPedido()
